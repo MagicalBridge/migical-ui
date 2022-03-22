a9a9d2bbf5c07bf3e4e9990fbd42d4f2e091b441
褚鹏飞 <540788769@qq.com>
2022-03-22 15:46:30 +0800

    faet: btn 组件初步实现

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, ButtonSize } from "./components/Button/button"
+// import logo from './logo.svg';
+// import './App.css';
+import './styles/index.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button disabled>hello</Button>
+        <Button btnType={ButtonType.Primary}>Primary hello</Button>
+        <Button size={ButtonSize.Small} btnType={ButtonType.Danger}>Small hello</Button>
+        <Button size={ButtonSize.Large}>Large hello</Button>
+        <Button btnType={ButtonType.Link} href="www.baidu.com">baidu</Button>
+        <Button disabled btnType={ButtonType.Link} href="www.baidu.com">baidu</Button>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
